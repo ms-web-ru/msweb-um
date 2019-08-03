5ec26763f6161944d28b84ca9e7071ae02264f9b
mixa86 <webmaster@ms-web.ru>
2019-08-03 14:47:45 +0500

    добавил анимацию и мд5

--- msweb-um.js
+++ msweb-um.js
@@ -54,7 +54,7 @@
 		var width = parent ? parent.width() : window.innerWidth;
 		var height = parent ? parent.height() : window.innerHeight;
 		bg = this.loader ? this.loader.bg : document.createElement('div');
-		var z = parent && parent[0].style.zIndex && parent[0].style.zIndex + 1 || 1111;
+		var z = parent && parent[0] && parent[0].style && parent[0].style.zIndex && parent[0].style.zIndex + 1 || 999995;
 		var parentPos = parent ? parent[0].getBoundingClientRect() : false;
 		var info = params.info || '';
 
@@ -88,7 +88,7 @@
 				bg: bg,
 				img: img
 			};
-			this.loader.info = $('<p style="z-index: 1111;display: block;position: relative;text-align: center;width: 100%;top: 80px;">' + info + '</p>')
+			this.loader.info = $('<p style="z-index: 999996;display: block;position: relative;text-align: center;width: 100%;top: 80px;">' + info + '</p>')
 			parent.append(this.loader.info);
 			this.loader.setInfo = function (info) {
 				this.loader.info.html(info);
@@ -286,7 +286,7 @@
 				var left = width / 2 + 5;
 				A.tooltipContainer.css({
 					"top": ev.clientY + window.scrollY + 20,
-					"left": (ev.clientX - left) > 0 ? ev.clientX - left  : 0
+					"left": (ev.clientX - left) > 0 ? ev.clientX - left : 0
 				})
 					.show();
 			} else {
@@ -345,6 +345,149 @@
 		return re.test(String(email).toLowerCase());
 	};
 
+	/**
+	 * Получение случайного целого числа
+	 * @param min
+	 * @param max
+	 * @returns {*}
+	 */
+	MSweb.prototype.rand = function (min, max) {
+		max--;
+		return Math.floor(Math.random() * (max - min)) + min;
+	};
 
-	msweb = new MSweb();
+
+	/**
+	 * Простой таймер обратного отсчёта
+	 * @param duration - кол-во секунд до конца отсчёта
+	 * @param container
+	 * @returns {Console | void}
+	 */
+	MSweb.prototype.simpleTimer = function (duration, container) {
+		container = container || document.querySelector('#ms-simple-timer');
+		if (!duration || !container)
+			return window.console && console.warn('SimpleTimer - empty required parameters');
+		var timer = duration, minutes, seconds, hours;
+		setInterval(function () {
+			hours = parseInt(timer / 60 / 60, 10);
+			minutes = parseInt((timer / 60) % 60, 10);
+			seconds = parseInt(timer % 60, 10);
+
+			minutes = minutes < 10 ? "0" + minutes : minutes;
+			seconds = seconds < 10 ? "0" + seconds : seconds;
+
+			container.textContent = hours + ':' + minutes + ":" + seconds;
+
+			if (--timer < 0) {
+				timer = duration;
+			}
+		}, 1000);
+	};
+
+	/**
+	 * Анимация случайных чисел в контейнере перед вставкой результата.
+	 * @param target - куда вставляется результат
+	 * @param opt_value - значение (опционально)
+	 * если не передано значение, тогда надо передавать минимальное и максимальное:
+	 * @param opt_min
+	 * @param opt_max
+	 * @param opt_timeout - время анимации. По умолчанию 1 секунда
+	 * @param opt_speed - 'slow', 'medium', 'fast' скорость анимации по умолчанию medium
+	 */
+	MSweb.prototype.animateRandom = function (target, opt_value, opt_min, opt_max, opt_timeout, opt_speed) {
+		var A = this;
+		opt_min = opt_min || 0;
+		opt_max = opt_max || 1000;
+		opt_max += 2;
+		opt_value = opt_value || this.rand(opt_min, opt_max);
+		if (String(opt_value).length < String(opt_max).length)
+			opt_max = opt_value;
+		var tempVal = A.rand(opt_min, opt_max);
+		opt_speed = opt_speed == 'slow' ? 100 : (opt_speed == 'medium' || !opt_speed ? 50 : '');
+
+		if (opt_speed) {
+			var interval = setInterval(function () {
+				tempVal = A.rand(opt_min, opt_max);
+			}, 50);
+		}
+		$({numberValue: opt_min}).animate({numberValue: opt_value}, {
+			duration: opt_timeout || 1000,
+			easing: "linear",
+			step: function (val) {
+				target.innerHTML = tempVal;
+			},
+			complete: function () {
+				target.innerHTML = opt_value;
+				if (opt_speed) {
+					clearInterval(interval);
+				}
+				console.log(opt_value);
+			}
+		});
+	};
+
+	MSweb.prototype.MD5 = function (d) {
+		function M(d) {
+			for (var _, m = "0123456789ABCDEF", f = "", r = 0; r < d.length; r++) _ = d.charCodeAt(r), f += m.charAt(_ >>> 4 & 15) + m.charAt(15 & _);
+			return f;
+		}
+
+		function X(d) {
+			for (var _ = Array(d.length >> 2), m = 0; m < _.length; m++) _[m] = 0;
+			for (m = 0; m < 8 * d.length; m += 8) _[m >> 5] |= (255 & d.charCodeAt(m / 8)) << m % 32;
+			return _;
+		}
+
+		function V(d) {
+			for (var _ = "", m = 0; m < 32 * d.length; m += 8) _ += String.fromCharCode(d[m >> 5] >>> m % 32 & 255);
+			return _;
+		}
+
+		function Y(d, _) {
+			d[_ >> 5] |= 128 << _ % 32, d[14 + (_ + 64 >>> 9 << 4)] = _;
+			for (var m = 1732584193, f = -271733879, r = -1732584194, i = 271733878, n = 0; n < d.length; n += 16) {
+				var h = m, t = f, g = r, e = i;
+				f = md5_ii(f = md5_ii(f = md5_ii(f = md5_ii(f = md5_hh(f = md5_hh(f = md5_hh(f = md5_hh(f = md5_gg(f = md5_gg(f = md5_gg(f = md5_gg(f = md5_ff(f = md5_ff(f = md5_ff(f = md5_ff(f, r = md5_ff(r, i = md5_ff(i, m = md5_ff(m, f, r, i, d[n + 0], 7, -680876936), f, r, d[n + 1], 12, -389564586), m, f, d[n + 2], 17, 606105819), i, m, d[n + 3], 22, -1044525330), r = md5_ff(r, i = md5_ff(i, m = md5_ff(m, f, r, i, d[n + 4], 7, -176418897), f, r, d[n + 5], 12, 1200080426), m, f, d[n + 6], 17, -1473231341), i, m, d[n + 7], 22, -45705983), r = md5_ff(r, i = md5_ff(i, m = md5_ff(m, f, r, i, d[n + 8], 7, 1770035416), f, r, d[n + 9], 12, -1958414417), m, f, d[n + 10], 17, -42063), i, m, d[n + 11], 22, -1990404162), r = md5_ff(r, i = md5_ff(i, m = md5_ff(m, f, r, i, d[n + 12], 7, 1804603682), f, r, d[n + 13], 12, -40341101), m, f, d[n + 14], 17, -1502002290), i, m, d[n + 15], 22, 1236535329), r = md5_gg(r, i = md5_gg(i, m = md5_gg(m, f, r, i, d[n + 1], 5, -165796510), f, r, d[n + 6], 9, -1069501632), m, f, d[n + 11], 14, 643717713), i, m, d[n + 0], 20, -373897302), r = md5_gg(r, i = md5_gg(i, m = md5_gg(m, f, r, i, d[n + 5], 5, -701558691), f, r, d[n + 10], 9, 38016083), m, f, d[n + 15], 14, -660478335), i, m, d[n + 4], 20, -405537848), r = md5_gg(r, i = md5_gg(i, m = md5_gg(m, f, r, i, d[n + 9], 5, 568446438), f, r, d[n + 14], 9, -1019803690), m, f, d[n + 3], 14, -187363961), i, m, d[n + 8], 20, 1163531501), r = md5_gg(r, i = md5_gg(i, m = md5_gg(m, f, r, i, d[n + 13], 5, -1444681467), f, r, d[n + 2], 9, -51403784), m, f, d[n + 7], 14, 1735328473), i, m, d[n + 12], 20, -1926607734), r = md5_hh(r, i = md5_hh(i, m = md5_hh(m, f, r, i, d[n + 5], 4, -378558), f, r, d[n + 8], 11, -2022574463), m, f, d[n + 11], 16, 1839030562), i, m, d[n + 14], 23, -35309556), r = md5_hh(r, i = md5_hh(i, m = md5_hh(m, f, r, i, d[n + 1], 4, -1530992060), f, r, d[n + 4], 11, 1272893353), m, f, d[n + 7], 16, -155497632), i, m, d[n + 10], 23, -1094730640), r = md5_hh(r, i = md5_hh(i, m = md5_hh(m, f, r, i, d[n + 13], 4, 681279174), f, r, d[n + 0], 11, -358537222), m, f, d[n + 3], 16, -722521979), i, m, d[n + 6], 23, 76029189), r = md5_hh(r, i = md5_hh(i, m = md5_hh(m, f, r, i, d[n + 9], 4, -640364487), f, r, d[n + 12], 11, -421815835), m, f, d[n + 15], 16, 530742520), i, m, d[n + 2], 23, -995338651), r = md5_ii(r, i = md5_ii(i, m = md5_ii(m, f, r, i, d[n + 0], 6, -198630844), f, r, d[n + 7], 10, 1126891415), m, f, d[n + 14], 15, -1416354905), i, m, d[n + 5], 21, -57434055), r = md5_ii(r, i = md5_ii(i, m = md5_ii(m, f, r, i, d[n + 12], 6, 1700485571), f, r, d[n + 3], 10, -1894986606), m, f, d[n + 10], 15, -1051523), i, m, d[n + 1], 21, -2054922799), r = md5_ii(r, i = md5_ii(i, m = md5_ii(m, f, r, i, d[n + 8], 6, 1873313359), f, r, d[n + 15], 10, -30611744), m, f, d[n + 6], 15, -1560198380), i, m, d[n + 13], 21, 1309151649), r = md5_ii(r, i = md5_ii(i, m = md5_ii(m, f, r, i, d[n + 4], 6, -145523070), f, r, d[n + 11], 10, -1120210379), m, f, d[n + 2], 15, 718787259), i, m, d[n + 9], 21, -343485551), m = safe_add(m, h), f = safe_add(f, t), r = safe_add(r, g), i = safe_add(i, e)
+			}
+			return Array(m, f, r, i);
+		}
+
+		function md5_cmn(d, _, m, f, r, i) {
+			return safe_add(bit_rol(safe_add(safe_add(_, d), safe_add(f, i)), r), m);
+		}
+
+		function md5_ff(d, _, m, f, r, i, n) {
+			return md5_cmn(_ & m | ~_ & f, d, _, r, i, n);
+		}
+
+		function md5_gg(d, _, m, f, r, i, n) {
+			return md5_cmn(_ & f | m & ~f, d, _, r, i, n);
+		}
+
+		function md5_hh(d, _, m, f, r, i, n) {
+			return md5_cmn(_ ^ m ^ f, d, _, r, i, n);
+		}
+
+		function md5_ii(d, _, m, f, r, i, n) {
+			return md5_cmn(m ^ (_ | ~f), d, _, r, i, n);
+		}
+
+		function safe_add(d, _) {
+			var m = (65535 & d) + (65535 & _);
+			return (d >> 16) + (_ >> 16) + (m >> 16) << 16 | 65535 & m;
+		}
+
+		function bit_rol(d, _) {
+			return d << _ | d >>> 32 - _;
+		}
+
+		var result = M(V(Y(X(d), 8 * d.length)));
+		return result.toLowerCase();
+	};
+
+
+	if (window.msweb)
+		msweb = Object.assign(new MSweb(), msweb);
+	else
+		msweb = new MSweb();
 })(jQuery);
